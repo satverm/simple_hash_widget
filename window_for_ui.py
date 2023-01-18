1fb659ebf0601373f3a505e8cd7cb6bdeb6ed397
satverm <64876484+satverm@users.noreply.github.com>
2023-01-18 23:15:07 +0530

    comments added

--- window_for_ui.py
+++ window_for_ui.py
@@ -27,6 +27,13 @@ Go the main window for storing more inputs or retrieving input from hash list
 All the functions shoule be in one file and made so generic that they can be called by
 the widgets as per requirement from the ui file
 
+## Lessons learnt 
+    For a gui with multiwidow workflow we should plan as under:
+        - Making the modules for standard functions: This will include all standard functions and without the need for any user input. Like hash(input), read_yn() etc.
+        - every window should be an independent window with it's widgets in teh window design as a group and the related functions immediately above the widgets design. This way everty window can be made modular and can be used at more than one place or can be copied and modified for similar requirements.
+        - For a fully gui based application, there should not be any input from the user in the terminal like app and thinking that the terminal is not available.
+        
+
 Title
 '''
 import tkinter as tk
